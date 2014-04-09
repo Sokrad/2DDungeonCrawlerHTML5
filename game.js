575ba9d109090b235cc2ce94ff752b6dc5bac5d4
Sokrad <odin@planetodin.de>
2014-04-09 19:11:56 +0200

    Draw Background
    Draw Background
    include Background Images
    
    Prototype from Map

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ var isPlaying = false;
 
 // GameObject-Array
 var gameObjects = new Array();
+var map = new Map();
 
 init();
 
@@ -17,30 +18,44 @@ function init()
 	}	
 	
 	//Init Canvas
-	//background_canvas = document.getElementById('background_canvas');
-	//backround_ctx = main_canvas.getContext('2d');
+	background_canvas = document.getElementById('background_canvas');
+	backround_ctx = background_canvas.getContext('2d');
 	main_canvas = document.getElementById('main_canvas');
 	main_ctx = main_canvas.getContext('2d');
 	
 	//Input Events Init
 	document.addEventListener("keydown", key_down, false);
+	document.addEventListener("keyup", key_up, false);
 	
 	requestaframe = (function() {
 					return window.requestAnimationFrame     	||
 						window.webkitRequestAnimationFrame    	||
-						window.mozReuestAnimationFrame        	||
+						window.mozRequestAnimationFrame        	||
 						window.oRequestAnimationFrame         	||
 						window.msRequestAnimationFrame        	||
 						function (callback) {
 							window.setTimeout(callback, 1000 / 60)
 					};
 	})();
+	
+	
+			
+	load_media();
 		
 	//Create new Player
 	player = new Player();
+	map.canvas = backround_ctx;
 	
 	//Add Player To Draw and Update array
 	gameObjects.push(player);
+	
+	
+}
+
+
+function load_media()
+{
+	map.block_sprite.src = 'image/floor_sprite.png';  
 }
 
 // Debug Mausposition
@@ -60,13 +75,13 @@ function gameLoop()
 	
 	gameUpdate();
 	gameDraw();
-	
-	
+		
 	if(isPlaying)
 		requestaframe(gameLoop);
 
 }
 
+//Game Update
 function gameUpdate()
 {
 	for(var i = 0;i<gameObjects.length;i++)
@@ -75,6 +90,7 @@ function gameUpdate()
 	}
 }
 
+//Game Draw
 function gameDraw()
 {
 	for(var i = 0;i<gameObjects.length;i++)
@@ -86,6 +102,8 @@ function gameDraw()
 //GameLoop Toggle Funktion
 function toggle_Loop()
 {
+	map.draw();
+	
 	if(!isPlaying)
 	{
 		isPlaying = true;
@@ -131,6 +149,58 @@ function key_down(e)
   }
 }
 
+function key_up(e)
+{
+  var key_id = e.keyCode || e.which;
+  if (key_id == 40) //down key
+  {
+    player.is_downkey = false;
+    e.preventDefault();
+  }
+  if (key_id == 38) //up key
+  {
+    player.is_upkey = false;
+    e.preventDefault();
+  }
+  if (key_id == 37) //left key
+  {
+    player.is_leftkey = false;
+    e.preventDefault();
+  }
+  if (key_id == 39) //right key
+  {
+    player.is_rightkey = false;
+    e.preventDefault();
+  }
+}
+
+// Map Prototype
+function Map()
+{
+	this.block_sprite = new Image();
+	this.block_count = 2;
+	this.blockwidth = 32;
+	this.blockheight = 32;
+	this.fw = 23;
+	this.fh = 17;
+	
+	this.BaseMap = new Array();
+	this.canvas = null;
+}
+Map.prototype.draw = function()
+{
+	var startx = 32;
+	var starty = 32;
+	for(var x=0;x<this.fw;x++)
+	{
+		for(var y=0;y<this.fh;y++)
+		{
+			map.canvas.drawImage(this.block_sprite,0,0,this.blockwidth,this.blockheight, startx+(x*this.blockwidth), starty+(y*this.blockheight), this.blockwidth, this.blockheight);
+		}
+	}
+};
+
+// Player Prototype
 function Player()
 {
   this.drawX = 0;
@@ -152,6 +222,13 @@ Player.prototype.update = function()
 };
 Player.prototype.check_keys = function()
 {
-  if (this.is_downkey == true)
-    this.drawY++;
+	if (this.is_downkey == true)
+		this.drawY++;
+	if (this.is_upkey == true)
+		this.drawY--;
+	if (this.is_rightkey == true)
+		this.drawX++;
+	if (this.is_leftkey == true)
+		this.drawX--;
 };
+
